6cf9bcc9960f87224d1c55e19610fd6c99665b2f
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2015-11-25 02:29:56 -0500

    Initial Commit. Includes working logic. Needs work on UI

---
+++ app/src/main/java/com/grojuspixels/spectrum/spectrum/CameraActivity.java
@@ -0,0 +1,19 @@
+package com.grojuspixels.spectrum.spectrum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
+    }
+
+}
